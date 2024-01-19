8c256e3cdf84b5354b698aaf64c1e4e2a92084c7
-3σ <yurippe@m3sigma.net>
2024-01-20 00:30:18 +0900

    priv

--- dataset/presets.py
+++ dataset/presets.py
@@ -8,3 +8,4 @@ class DatasetPreset(Enum):
     WOS_S = DatasetInfo(GRAPH_WOS_S, "aeromaki/WOS5736", 3)
     WOS_M = DatasetInfo(GRAPH_WOS_M, "aeromaki/WOS11967", 3)
     WOS_L = DatasetInfo(GRAPH_WOS_L, "aeromaki/WOS46985", 3)
+    PRIV_01 = DatasetInfo(GRAPH_PRIV_01, "aeromaki/htc-private-01", 4)
